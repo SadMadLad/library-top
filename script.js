e758073a1a654f7cf6a8470b1e07d152a387c177
Saad Azam <zeroinside4u@gmail.com>
2022-10-21 20:16:55 +0500

    Flexbox has been set

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const books = [];
+let bookCounter = 1;
 
-function book(author, title, pages, isRead){
+function book(id, title, author, pages, isRead, imageURL){
+    this.id = id;
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.isRead = isRead;
+    this.imageURL = imageURL;
     this.toggleRead = () => {
         this.isRead = !this.isRead;
     }
@@ -14,18 +17,26 @@ function book(author, title, pages, isRead){
     }
     this.makeHTMLBook = () => {
         const titleTag = document.createElement("h2");
-        const authorTag = document.createElement("p");
+        const authorTag = document.createElement("h3");
         const pagesTag = document.createElement("p");
-        const isReadTag = document.createElement("button");
-        const imageTag = document.createElement("div");
+        const isReadTag = document.createElement("button");        
+        const imageTag = document.createElement("img");
+
+        const detailsDiv = document.createElement("div");
+        const imageDiv = document.createElement("div");
 
         titleTag.textContent = this.title;
         authorTag.textContent = this.author; 
         pagesTag.textContent = `${this.pages}`;
         isReadTag.textContent = `${this.isRead}`;
 
+        imageTag.setAttribute('src', this.imageURL);
+        imageDiv.appendChild(imageTag);
+        imageDiv.classList.add('image');
+        detailsDiv.classList.add('book-details');
+        detailsDiv.append(titleTag, authorTag, pagesTag, isReadTag);
         const bookDiv = document.createElement('div');
-        bookDiv.append(titleTag, authorTag, pagesTag, isReadTag);
+        bookDiv.append(detailsDiv, imageDiv);
         bookDiv.classList.add('book');
 
         console.log(bookDiv);
@@ -40,11 +51,25 @@ const renderBook = (book) => {
     booksDiv.appendChild(bookDiv);
 }
 
-const english = new book('a', 'b', 12, false);
-console.log(english.bookInfo());
-english.toggleRead();
-console.log(english.bookInfo());
-english.makeHTMLBook();
+const english = new book(0, 'English Book', 'Pagal Aadmi', 132, false, `https://source.unsplash.com/random/${bookCounter}`);
+bookCounter++;
+const biology = new book(1, 'Biology', 'English Man', 200, false, `https://source.unsplash.com/random/${bookCounter}`);
+bookCounter++;
+const geology = new book(2, 'Geology of the Forest', 'Best Biology Author', 300, false, `https://source.unsplash.com/random/${bookCounter}`);
+bookCounter++;
+const weed = new book(3, 'How to smoke Weed', 'Nate Diaz', 2000, false, `https://source.unsplash.com/random/${bookCounter}`);
+bookCounter++;
+const tobacco = new book(4, 'Tobacco = Money', 'Dogfather Hermannson', 10210, true, `https://source.unsplash.com/random/${bookCounter}`);
+
 renderBook(english);
+renderBook(biology);
+renderBook(geology);
+renderBook(weed);
+renderBook(tobacco);
+// console.log(english.bookInfo());
+// english.toggleRead();
+// console.log(english.bookInfo());
+// english.makeHTMLBook();
+// renderBook(english);
 //const bookAdderButton = document.getElementsByClassName('add-book');
 
