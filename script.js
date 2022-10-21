225f801fd0fb13ef41794768483f1b95f5d6b8c6
Saad Azam <zeroinside4u@gmail.com>
2022-10-21 22:16:40 +0500

    Set up Helper and Book object function

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const books = [];
-let bookCounter = 1;
+let bookCounter = 0;
 
+let books = [
+    new book(0, 'English Book', 'Pagal Aadmi', 132, false, `https://source.unsplash.com/random/${bookCounter}`),
+    new book(1, 'Biology', 'English Man', 200, false, `https://source.unsplash.com/random/${bookCounter}`),
+    new book(2, 'Geology of the Forest', 'Best Biology Author', 300, false, `https://source.unsplash.com/random/${bookCounter}`),
+    new book(3, 'How to smoke Weed', 'Nate Diaz', 2000, false, `https://source.unsplash.com/random/${bookCounter}`),
+    new book(4, 'Tobacco = Money', 'Dogfather Hermannson', 10210, true, `https://source.unsplash.com/random/${bookCounter}`),
+];
+
+//Helper Functions
+const handleBookDelete = (e) => {
+    let deleteID = e.target.getAttribute('data-id'); 
+    deleteID = parseInt(deleteID);
+    for(let i=0; i<books.length; i++){
+        if(deleteID === books[i].id){
+            books.splice(i, 1);
+            break;
+        }
+    };
+    const booksDiv = document.getElementById("books");
+    booksDiv.replaceChildren();
+    renderBooks(books);
+};
+const renderBooks = (booksList) => {
+    const booksDiv = document.getElementById('books');
+    booksList.forEach(book => {
+        const bookDiv = book.makeHTMLBook();
+        booksDiv.appendChild(bookDiv);
+    });
+};
+
+
+//Book Object
 function book(id, title, author, pages, isRead, imageURL){
     this.id = id;
     this.author = author;
@@ -11,65 +42,62 @@ function book(id, title, author, pages, isRead, imageURL){
     this.toggleRead = () => {
         this.isRead = !this.isRead;
     }
+    this.returnRead = () => {
+        return (this.isRead ? 'Yes' : 'No');
+    }
     this.bookInfo = () => {
         const info = `Author: ${this.author}\nTitle: ${this.title}\nPages: ${this.pages}\nAlready Read: ${this.isRead}`;
         return info;
     }
+    // Makes the HTML out of the object
     this.makeHTMLBook = () => {
         const titleTag = document.createElement("h2");
         const authorTag = document.createElement("h3");
         const pagesTag = document.createElement("p");
-        const isReadTag = document.createElement("button");        
+        const isReadTag = document.createElement("button");
+        const deleteButton = document.createElement("button");        
         const imageTag = document.createElement("img");
 
         const detailsDiv = document.createElement("div");
         const imageDiv = document.createElement("div");
+        const buttonsDiv = document.createElement("div");
+        const alreadyRead = this.isRead ? 'Yes' : 'No';
 
         titleTag.textContent = this.title;
         authorTag.textContent = this.author; 
         pagesTag.textContent = `${this.pages}`;
-        isReadTag.textContent = `${this.isRead}`;
+        isReadTag.textContent = `Read: ${alreadyRead}`;
+        deleteButton.textContent = `Remove Book`;
 
         imageTag.setAttribute('src', this.imageURL);
         imageDiv.appendChild(imageTag);
         imageDiv.classList.add('image');
+
+        deleteButton.setAttribute('data-id', this.id);
+        deleteButton.addEventListener('click', handleBookDelete);
+        isReadTag.addEventListener('click', this.readToggleEvent);
+        if(this.isRead === true){
+            isReadTag.classList.add('read');
+        }
+        buttonsDiv.classList.add('buttons');
+        buttonsDiv.append(isReadTag, deleteButton);
+        
         detailsDiv.classList.add('book-details');
-        detailsDiv.append(titleTag, authorTag, pagesTag, isReadTag);
+        detailsDiv.append(titleTag, authorTag, pagesTag, buttonsDiv);
+
         const bookDiv = document.createElement('div');
         bookDiv.append(detailsDiv, imageDiv);
         bookDiv.classList.add('book');
 
-        console.log(bookDiv);
-
         return bookDiv;
+    },
+    //Switch between Read: Yes and No.
+    this.readToggleEvent = (e) => {
+        this.toggleRead();
+        const read = this.returnRead();
+        e.target.textContent = `Read: ${read}`;
+        e.target.classList.toggle('read');
     }
-}
+};
 
-const renderBook = (book) => {
-    const bookDiv = book.makeHTMLBook();
-    const booksDiv = document.getElementById('books');
-    booksDiv.appendChild(bookDiv);
-}
-
-const english = new book(0, 'English Book', 'Pagal Aadmi', 132, false, `https://source.unsplash.com/random/${bookCounter}`);
-bookCounter++;
-const biology = new book(1, 'Biology', 'English Man', 200, false, `https://source.unsplash.com/random/${bookCounter}`);
-bookCounter++;
-const geology = new book(2, 'Geology of the Forest', 'Best Biology Author', 300, false, `https://source.unsplash.com/random/${bookCounter}`);
-bookCounter++;
-const weed = new book(3, 'How to smoke Weed', 'Nate Diaz', 2000, false, `https://source.unsplash.com/random/${bookCounter}`);
-bookCounter++;
-const tobacco = new book(4, 'Tobacco = Money', 'Dogfather Hermannson', 10210, true, `https://source.unsplash.com/random/${bookCounter}`);
-
-renderBook(english);
-renderBook(biology);
-renderBook(geology);
-renderBook(weed);
-renderBook(tobacco);
-// console.log(english.bookInfo());
-// english.toggleRead();
-// console.log(english.bookInfo());
-// english.makeHTMLBook();
-// renderBook(english);
-//const bookAdderButton = document.getElementsByClassName('add-book');
-
+renderBooks(books);
